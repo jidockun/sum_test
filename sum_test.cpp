bb0b6c1f4814d4356ef67b90bd921027395c6a67
jidockun <67495873+jidockun@users.noreply.github.com>
2020-07-09 11:16:55 +0900

    Update sum_test.cpp

--- sum_test.cpp
+++ sum_test.cpp
@@ -1,9 +1,14 @@
 #include "sum_test.h"
 
 int sum(int n) {
+	// assured that n*(n+1) is even
+	// so that it's divisible by 2
+	return n*(n+1)/2;
+	/*
 	int ret = 0;
 	for(int i = 1; i <= n; i++) {
 		ret += i;
 	}
 	return ret;
+	*/
 }
